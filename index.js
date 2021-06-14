14250cd9364ac225351c406a9b3720bc040fbab7
Fabien Ropert <fabien-ropert@outlook.fr>
2021-06-14 15:30:41 +0200

    peuplement de la base de données

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db.db', (err) => {
+    if(err) {
+      return console.error(err.message);
+    }
+    console.log('Connecté à la base de données');
+  });
 
+var i = 1;
+
+const results = [];
 fs.createReadStream('data.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    console.log(row);
+  .pipe(csv({ separator: ';' }))
+  .on('data', (data) => {
+    db.run(`INSERT INTO Gestion(id,identifiant,numeroLot,hour,valeur) VALUES(?,?,?,?,?)`, [i,data.id,data.numeroLot,data.hour,data.value] , function(err) {
+        if(err) {
+            return console.log(err.message);
+        }
+    });
+    i++;
   });
