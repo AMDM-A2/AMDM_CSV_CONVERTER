a488903f2d53c535bc965a96917157961445fcfb
Fabien Ropert <fabien-ropert@outlook.fr>
2021-06-16 12:56:11 +0200

    modification script de propagation

--- index.js
+++ index.js
@@ -2,18 +2,56 @@ const csv = require('csv-parser');
 const fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db.db', (err) => {
-    if(err) {
-      return console.error(err.message);
+    if (err) {
+        return console.error(err.message);
     }
     console.log('Connecté à la base de données');
-  });
+});
 
 fs.createReadStream('data.csv')
-  .pipe(csv({ separator: ';' }))
-  .on('data', (data) => {
-    db.run(`INSERT INTO Gestion(identifiant,numeroLot,hour,valeur) VALUES(?,?,?,?)`, [data.id,data.numeroLot,data.hour,data.value] , function(err) {
-        if(err) {
-            return console.log(err.message);
+    .pipe(csv({
+        separator: ';'
+    }))
+    .on('data', (data) => {
+        
+        //remplissage des types de produit
+        if(data.id != 'alert') {
+            db.run(`INSERT OR IGNORE INTO TypesProduits(libelle) VALUES(?);`, [data.id], function(err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
         }
+
+        //remplissage des lots
+        db.run(`INSERT OR IGNORE INTO Lots(id) VALUES(?);`, [data.numeroLot], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+        });
+
+        if(data.id == 'alert') {
+            db.run(`INSERT INTO Alertes(numeroLot,heure,valeur) VALUES(?,?,?)`, [data.numeroLot,data.hour,data.value] , function(err) {
+                if(err) {
+                    return console.log(err.message);
+                }
+            });
+        } else {
+
+            db.get("SELECT id FROM TypesProduits WHERE libelle = ?", [data.id], (err, row) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                db.run(`INSERT INTO Produits(typeProduit,numeroLot,heure,valeur) VALUES(?,?,?,?)`, [row.id,data.numeroLot,data.hour,data.value] , function(err) {
+                    if(err) {
+                        return console.log(err.message);
+                    }
+                });
+            });
+
+
+            
+        }
+
+
     });
-  });
